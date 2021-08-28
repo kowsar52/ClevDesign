992d7b0b6f591295a5966904aa6675ac641b9216
hrrahman678 <89016443+hrrahman678@users.noreply.github.com>
2021-08-28 10:03:43 +0300

    update

--- New Project1/js/main.js
+++ New Project1/js/main.js
@@ -1,3 +1,35 @@
-$(".toggleSideBtn").click(function(){
-    $("body").toggleClass("hide-sidebar")
+$(document).ready(function(){
+    triggerButton();
+    dropdown();
 })
+
+
+function triggerButton(){
+    $(".triggerButton").on('click',function(){
+        $("body").toggleClass("hide-sidebar")
+    })
+}
+
+
+function dropdown(){
+    $(".dropdown-trigger").on('click',function(){
+
+        if( $(".dropdown-trigger").next().hasClass('dropdown-show')){
+            $(document).find('.dropdown-menu.dropdown-show').removeClass('dropdown-show')
+        }
+
+            $(this).next('.dropdown-menu').toggleClass('dropdown-show')
+            
+        
+
+
+        
+    })
+}
+
+
+// function dropdown(){
+//     $(".dropdown-trigger").on('click',function(){
+//         $(".notification").toggleClass("dropdown-show")
+//     })
+// }
