daa2776f66a34eb2241d983184df74604f5446ad
hrrahman678 <89016443+hrrahman678@users.noreply.github.com>
2021-08-31 20:55:55 +0300

    Full Update

--- New Project1/js/main.js
+++ New Project1/js/main.js
@@ -18,8 +18,12 @@ function dropdown(){
         
         if( $(this).next().hasClass('dropdown-show')){
             $(document).find('.dropdown-menu.dropdown-show').removeClass('dropdown-show')
+            $(document).find('.rotate_90_deg').removeClass('rotate_90_deg')
         }else{
+            $(document).find('.rotate_90_deg').removeClass('rotate_90_deg')
+            $(this).find('rotate-icon').addClass('rotate_90_deg')
 
+            
             if( $(".dropdown-trigger").next().hasClass('dropdown-show')){
                 $(document).find('.dropdown-menu.dropdown-show').removeClass('dropdown-show')
             }
