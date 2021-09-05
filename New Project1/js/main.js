36cedb4ee42777d0def86b4a487a59b54b430062
hrrahman678 <89016443+hrrahman678@users.noreply.github.com>
2021-09-05 10:08:07 +0300

    update

--- New Project1/js/main.js
+++ New Project1/js/main.js
@@ -19,16 +19,23 @@ function dropdown(){
         if( $(this).next().hasClass('dropdown-show')){
             $(document).find('.dropdown-menu.dropdown-show').removeClass('dropdown-show')
             $(document).find('.rotate_90_deg').removeClass('rotate_90_deg')
+            $(document).find('.dropddown-trigger.active').removeClass('active')
         }else{
             $(document).find('.rotate_90_deg').removeClass('rotate_90_deg')
-            $(this).find('rotate-icon').addClass('rotate_90_deg')
+            $(this).find('.rotate-icon').addClass('rotate_90_deg')
 
             
             if( $(".dropdown-trigger").next().hasClass('dropdown-show')){
                 $(document).find('.dropdown-menu.dropdown-show').removeClass('dropdown-show')
             }
+
+            if( $(".dropdown-trigger").hasClass('active')){
+                $(document).find('.dropdown-trigger.active').removeClass('active')
+            }
+
     
              $(this).next('.dropdown-menu').toggleClass('dropdown-show')
+             $(this).toggleClass('active')
                 
         }
     
