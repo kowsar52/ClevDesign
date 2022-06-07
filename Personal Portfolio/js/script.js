d5ff2189aa1b816309c7fc48f8d7eba36137f256
Kowsar Hossain <34295607+kowsar52@users.noreply.github.com>
2022-06-07 20:00:38 +0600

    1

---
+++ Personal Portfolio/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        items: 3,
+        loop: true,
+        nav:true,
+        margin: 30,
+        navContainer : '.next-prev',
+        navItems: ['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+        dotsContainer: '.scroll',
+        responsive : {
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:true
+            }
+        }
+    })
+});
